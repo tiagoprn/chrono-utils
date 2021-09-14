d45b695ded2c1a0c508eff0ef9badc7dfc1b2eb5
tiago.paranhos <tiagoprn@gmail.com>
2021-09-14 07:09:45 -0300

    feat: (wip) Starting implementation.

--- chronofilter/commands.py
+++ chronofilter/commands.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from sys import stdout, stdin
 
 import typer
@@ -9,8 +10,19 @@ app = typer.Typer()
 def filter_input(number_of_records: int):
     stdout.write(f'Will filter for the next {number_of_records} records.\n')
     data = stdin.read()
+
+    current_timestamp = datetime.now(
+            ).strftime('%I:%M%p').lower()
+
+    if current_timestamp.startswith('0'):
+        current_timestamp = current_timestamp[1:]
+
+    stdout.write('Filtering all records >= '
+                 f'{current_timestamp}...\n')
+
     for line in data:
         stdout.write(line)
+        # TODO: process the lines here
 
 
 if __name__ == '__main__':
