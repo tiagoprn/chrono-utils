9e541e5a94c89e64cc008150833a718e299631a3
tiago.paranhos <tiagoprn@gmail.com>
2021-09-14 07:17:56 -0300

    refac: extrated function to get the current timestamp in the desired format

--- chronofilter/commands.py
+++ chronofilter/commands.py
@@ -6,16 +6,20 @@ import typer
 app = typer.Typer()
 
 
+def _get_current_timestamp_in_12_hours_format() -> str:
+    current_timestamp = datetime.now(
+            ).strftime('%I:%M%p').lower()
+
+    return current_timestamp[1:] if current_timestamp.startswith(
+            '0') else current_timestamp
+
+
 @app.command()
 def filter_input(number_of_records: int):
     stdout.write(f'Will filter for the next {number_of_records} records.\n')
     data = stdin.read()
 
-    current_timestamp = datetime.now(
-            ).strftime('%I:%M%p').lower()
-
-    if current_timestamp.startswith('0'):
-        current_timestamp = current_timestamp[1:]
+    current_timestamp = _get_current_timestamp_in_12_hours_format()
 
     stdout.write('Filtering all records >= '
                  f'{current_timestamp}...\n')
