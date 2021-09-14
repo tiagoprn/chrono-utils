b40b6e5962dae86b17e49845be25e9e0e7989495
tiago.paranhos <tiagoprn@gmail.com>
2021-09-14 07:45:22 -0300

    feat: (wip) extracting a more focused function to filter the records.

--- chronofilter/commands.py
+++ chronofilter/commands.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from sys import stdout, stdin
+from typing import List
 
 import typer
 
@@ -14,10 +15,8 @@ def _get_current_timestamp_in_12_hours_format() -> str:
             '0') else current_timestamp
 
 
-@app.command()
-def filter_input(number_of_records: int):
-    stdout.write(f'Will filter for the next {number_of_records} records.\n')
-    data = stdin.read()
+def _filter_records(data: List[str], number_of_records: int) -> List[str]:
+    filtered_records = []
 
     current_timestamp = _get_current_timestamp_in_12_hours_format()
 
@@ -25,8 +24,21 @@ def filter_input(number_of_records: int):
                  f'{current_timestamp}...\n')
 
     for line in data:
-        stdout.write(line)
         # TODO: process the lines here
+        filtered_records.append(line)
+        if len(filtered_records) == number_of_records:
+            return filtered_records
+
+
+@app.command()
+def filter_input(number_of_records: int):
+    stdout.write(f'Will filter for the next {number_of_records} records.\n')
+    data = stdin.readlines()
+
+    # __import__('pudb').set_trace()
+    filtered_records = _filter_records(data, number_of_records)
+    for record in filtered_records:
+        stdout.write(record)
 
 
 if __name__ == '__main__':
