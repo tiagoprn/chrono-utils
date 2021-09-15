a8cd195e6d35c674c358e753795519527b213c53
tiago.paranhos <tiagoprn@gmail.com>
2021-09-15 07:09:59 -0300

    test: parametrized test to have more cases.

--- chronofilter/tests/test_commands.py
+++ chronofilter/tests/test_commands.py
@@ -1,13 +1,23 @@
 from datetime import datetime
 
+import pytest
 import time_machine
 
 from chronofilter.commands import _get_datetime_instance_from_12_hours_format
 
 
-def test_get_current_timestamp_in_12_hours_format():
-    fake_current_datetime = datetime.fromisoformat('2021-01-01T08:00')
+@pytest.mark.parametrize(
+        'fake_current_datetime,hour,expected_datetime',
+        [
+            ['2021-01-01T08:00', '9:00am', '2021-01-01T09:00'],
+            ['2021-01-01T08:00', '1:00pm', '2021-01-01T13:00'],
+            ['2021-01-01T08:00', '12:00am', '2021-01-01T00:00'],
+            ['2021-01-01T08:00', '12:00pm', '2021-01-01T12:00']
+        ]
+)
+def test_get_datetime_instance_from_12_hours_format(
+        fake_current_datetime, hour, expected_datetime):
     with time_machine.travel(fake_current_datetime):
-        hour = '9:00am'
         datetime_instance = _get_datetime_instance_from_12_hours_format(hour)
-        assert datetime_instance == datetime.fromisoformat('2021-01-01T09:00')
+        expected_datetime_instance = datetime.fromisoformat(expected_datetime)
+        assert datetime_instance == expected_datetime_instance
